d8fb1f5d4806f15810bd398e8242e1b1ef2a3e12
yisun2001 <y.sun6@student.avans.nl>
2020-10-19 15:05:24 +0200

    Added CSS styling for navbar, login page

--- Core.Domain/Client.cs
+++ Core.Domain/Client.cs
@@ -11,7 +11,7 @@ namespace Core.Domain
         public string UserName { get; set; }
 
         public string Password { get; set; }
-        public string Adress { get; set; }
+        public string Address { get; set; }
         public string PhoneNumber { get; set; }
         public DateTime DateOfBirth { get; set; }
 
